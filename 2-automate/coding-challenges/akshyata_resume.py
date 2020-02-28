ac91ed4296f56ffa36cadc64bb2787ba0fc25cdc
Akshyata Baskota <i156802@d2hs.com>
2020-02-28 11:16:24 +0545

    started coding challenge for resume generator

---
+++ 2-automate/coding-challenges/akshyata_resume.py
@@ -0,0 +1,25 @@
+#from pathlib import Path
+
+#p = Path('C:\Users\i156802\python-e2e-book\2-automate\coding-challenges')
+#p.mkdir(exist_ok=True)
+
+#class resume(object):
+
+#def __init__(self):
+        #self._database = {
+
+
+database ={ 'name': 'Akshyata','location': 'Imadol','phone': '984152xxxx','email': 'akshyata@gmail.com'}
+   
+
+for name, location, phone, email in database.items():
+    print(f'{name:10} /n {phone:10d} /n {location:10} /n {email:10}')
+
+
+
+#with open('Akshyata.md', 'w') as writer:
+ #   database = {'name': 'Akshyata','location': 'Imadol','phone': '984152xxxx','email': 'akshyata@gmail.com'}
+ #   for contact in contacts:
+ #       writer.write(contacts)
+
+ 
